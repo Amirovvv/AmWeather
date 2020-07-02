7a0e203a4c70be06260f490f7a7f2fd2ba4b5104
Amirovvv <58747052+Amirovvv@users.noreply.github.com>
2020-07-02 17:22:35 +0300

    relase v2

--- weather/views.py
+++ weather/views.py
@@ -1,12 +1,31 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 def index(request):
     appid = '04df3bd974e178dfde66e1f7428c5a7b'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
-    city = 'London'
-    res = requests.get(url.format(city))
-    print(res.text)
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
 
-    return render(request, 'weather/index.html')
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form':form}
+
+    return render(request, 'weather/index.html', context)
